d0282ff55fe51add511aa54e76c7646c5d9fd8d4
duecknoah <duecknoah@gmail.com>
2017-11-20 15:26:50 -0800

    Initial commit

---
+++ check_exams.py
@@ -0,0 +1,58 @@
+import urllib.request
+from bs4 import BeautifulSoup
+from prettytable import PrettyTable
+
+def table_to_list(table):
+    """Converts the HTML table to a list"""
+
+    data = []
+
+    for row in table.find_all('tr')[1:]:
+        cols = row.find_all('td')
+        cols = [ele.text.strip() for ele in cols]
+        data.append(cols)
+
+    return data
+
+def get_course_data(crns, all_course_data, crn_index=2):
+    """
+    Finds and returns the course #'s (CRNs) matching
+    in 'all_course_data'.
+    Where all_course_data is a list
+    courses is a list
+    """
+
+    c_data = []
+    for d in all_course_data:
+        for crn in crns:
+            if d[crn_index] == crn:
+                c_data.append(d)
+    return c_data
+
+def print_exams(headings, courses):
+    """Prints all info for your courses' exams"""
+    t = PrettyTable(headings)
+
+    for c in courses:
+        t.add_row(c)
+    print(t)
+
+if __name__ == '__main__':
+    # Course numbers you are taking
+    myCRNs = ['91910', '91918', '91995', '91997']
+    # The web page with the exam schedule
+    html_doc = urllib.request.urlopen('https://www.ufv.ca/arfiles/includes/201709-exam-schedule.htm')
+
+    soup = BeautifulSoup(html_doc, 'lxml')
+    table = soup.find('table')
+
+    # Make heading names reference the column index they refer to
+    # Ex. {'COURSE': 0, 'SECTION': 1, 'CRN': 2}
+    headings_text = table.find('tr').get_text().strip().split('\n')
+    headings = {}
+    for i, hName in enumerate(headings_text):
+        headings[hName] = i
+
+    all_c_data = table_to_list(table)
+    my_course_data = get_course_data(myCRNs, all_c_data, headings['CRN'])
+    print_exams(headings_text, my_course_data)
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='ufv_exam_schedule',
+      version='0.1',
+      description='',
+      author='Noah Dueck',
+      author_email='duecknoah@gmail.com',
+      license='MIT',
+      install_requires=[
+          'bs4', 'PrettyTable'
+      ],
+      zip_safe=False)
