bde83ac47f4234807b7b552b4498a24d16fb6da2
duecknoah <duecknoah@gmail.com>
2017-11-20 18:27:13 -0800

    Compacted setup.py, ignored files created by setup

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import setup
 
-setup(name='ufv_exam_schedule',
+setup(name='ufv_exam_schedule_checker',
       version='0.1',
       description='',
       author='Noah Dueck',
       author_email='duecknoah@gmail.com',
-      license='MIT',
       install_requires=[
           'bs4', 'PrettyTable'
       ],
