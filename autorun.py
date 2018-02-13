701d663eb31d24e0587724a31c203080d6490277
duecknoah <duecknoah@gmail.com>
2018-02-13 15:56:58 -0800

    autorun.py can now take in an argument for the seconds between checks, number formatting improved

--- autorun.py
+++ autorun.py
@@ -1,19 +1,20 @@
 """Start me and leave me to run
 
-This checks every 12 hours if any exam information has
-changed, and will notify via email the changes. Changes
-are also logged in the updates.log file
+This checks every n hours if any exam information has
+changed, changes are also logged in the exam_changes.log file
 """
 import threading
 import check_exams as exams
+import sys
 
-# The amount of seconds before re checking changes
-INTERVAL = 43200
-INTERVAL_AS_HOURS = INTERVAL / 60 / 60
-
-def check_for_updates():
-    threading.Timer(INTERVAL, check_for_updates).start()
+def check_for_updates(interval=43200):
+    """Checks for exam updates every 'interval' seconds"""
     exams.check()
-    print('Running again in {} hours'.format(INTERVAL_AS_HOURS))
+    threading.Timer(interval, check_for_updates, [interval]).start()
+    INTERVAL_AS_HOURS = interval / 60 / 60
+    print('Running again in {0:.4f} hours'.format(INTERVAL_AS_HOURS))
 
-check_for_updates()
+if __name__ == '__main__':
+    # The amount of seconds before re checking changes
+    INTERVAL = 43200 if (len(sys.argv) != 2) else int(sys.argv[1])
+    check_for_updates(INTERVAL)
